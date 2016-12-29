c718bfaac93046c297b51dd8635ddf8a7b5aadab
Vit S <fornit1917@gmail.com>
2016-12-29 23:41:43 +0300

    init

---
+++ js/config.js
@@ -0,0 +1,25 @@
+var Config = {
+    o: {
+        file:'o.png',
+        bg:'#2989cc',
+        snowColor:'#ffffff',
+        w: 600,
+        h: 703
+    },
+
+    m: {
+        file:'m.png',
+        bg:'#2989cc',
+        snowColor:'#ffffff',
+        w: 846,
+        h: 960
+    },
+
+    leo: {
+        file:'leo.png',
+        bg:'#3a3697',
+        snowColor:'#ffffff',
+        w: 555,
+        h: 767
+    },
+}
